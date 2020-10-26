3f5cbaf6b33c6af0e5d1f789dfecea9d6762f011
heyifeng226 <46611375+heyifeng226@users.noreply.github.com>
2020-10-25 23:19:24 -0700

    add CSS

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx";
+import styles from "../ArticleList/ArticleList.module.css";
 
 const ArticleList = (props) => {
   let displayContent;
 
   if (props.articles.length) {
     displayContent = (
-      <ul>
+      <div className={styles.article_list}>
         {props.articles.map((article) => (
           <ArticleListItem article={article} key={article.slug} />
         ))}
-      </ul>
+      </div>
     );
   } else {
     displayContent = <div>You have no data!</div>;
